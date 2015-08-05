a5c668fcd091960dc9472413f42d230ec69d5e46
ISHIDA Akio <ishida@coderium.com>
2015-08-05 18:43:42 +0900

    Add test.

--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -5,9 +5,20 @@ class ArrayTest extends PHPUnit_Framework_TestCase {
         return $arr;
     }
     function test_unset_and_append() {
+        $this->markTestIncomplete();
         $arr = array(0, 1);
         unset($arr[1]);
         $arr = $this->append($arr, 2);
+        $this->assertEquals(2, $arr[2]);
+    }
+    function return_array() {
+        $arr = array(0, 1);
+        unset($arr[1]);
+        return $arr;
+    }
+    function test_array_returns_array() {
+        $arr = $this->return_array();
+        $arr[] = 2;
         $this->assertEquals(2, $arr[2]);
     }
 }
